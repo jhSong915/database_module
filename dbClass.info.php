3fb528fb844da411994e94b30863d347c1b08a13
jhsong915 <jhsong915@naver.com>
2023-11-16 16:33:28 +0900

    SQL Connect Module

---
+++ dbClass.info.php
@@ -0,0 +1,10 @@
+<?php
+$dev_db_host = ''; #DataBase Server IP or Domain
+$dev_db_id = ''; #DataBase Connection id
+$dev_db_password = ''; #DataBase Connection Password
+$dev_db_database = ''; #Connection Default DataBase(Schema)
+
+define("DB_DEV_HOST", $dev_db_host);
+define("DB_DEV_ID", $dev_db_id);
+define("DB_DEV_PASS", $dev_db_password);
+define("DB_DEV_DATABASE", $dev_db_database);
